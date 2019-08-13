4216a73c9fdaff21b0706eb414d896e359eda3d0
Evgenii Smolnikov <esmolnikov@profit.org.ru>
2019-08-13 11:40:11 +0500

    set up project

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = props =>
+		<h1>{props.course}</h1>
+
+const Content = props => {
+	const parts = props.parts
+
+	return (
+		<>
+			<Part part={parts[0]} />
+			<Part part={parts[1]} />
+			<Part part={parts[2]} />
+		</>
+	)
+}
+
+const Total = props => {
+	const parts = props.parts
+
+	return (
+		<>
+			<p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+		</>
+	)
+}
+
+const Part = props =>
+		<p>{props.part.name} {props.part.exercises}</p>
+
+const App = () => {
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7
+			},
+			{
+				name: 'State of a component',
+				exercises: 14
+			}
+		]
+	}
+
+	return (
+		<>
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts}/>
+		</>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
