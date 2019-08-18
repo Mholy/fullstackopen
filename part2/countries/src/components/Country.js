759b5eae5d5e7fc21224b836eaca6bcae5d5d1d4
Eugene Leto <mholy@protonmail.ch>
2019-08-18 16:55:19 +0500

    2.14*: Data for countries, step3

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import { Weather } from './Weather'
 
-export const Country = ({ country }) => (
-    <>
-        <h2>{country.name}</h2>
-        <div>capital {country.capital}</div>
-        <div>population {country.population}</div>
-        <h3>Languages</h3>
-        <ul>
-            {country.languages.map(language => (
-                <li key={language.name}>{language.name}</li>
-            ))}
-        </ul>
-        <p>
-            <img src={country.flag} width="150" alt='country flag' />
-        </p>
-    </>
-)
+export const Country = ({ country }) => {
+    return (
+        <>
+            <h2>{country.name}</h2>
+            <div>capital {country.capital}</div>
+            <div>population {country.population}</div>
+            <h3>Languages</h3>
+            <ul>
+                {country.languages.map(language => (
+                    <li key={language.name}>{language.name}</li>
+                ))}
+            </ul>
+            <p>
+                <img src={country.flag} width='150' alt='country flag' />
+            </p>
+            <Weather country={country} />
+        </>
+    )
+}
